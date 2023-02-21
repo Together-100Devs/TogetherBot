fe668a9aa6e6377474a891187c69fb12e90496e7
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-02-21 18:52:26 -0500

    Added Embed DM slash command for single & all memebers

--- index.js
+++ index.js
@@ -22,59 +22,60 @@ client.on('ready', (c) => {
 	console.log(`✅ ${c.user.tag} is online`)
 });
 
-// client.on('messageCreate', (message, event ='Banki Time', time = '9:00pm EST', date = 'March 10, 2023') => {
-// 	if(message.author.bot){
-// 		return;
-// 	}
-	
-// 	if(message.content === 'Next Event'){
-// 		message.reply(`Hey! The next event is ${event} on ${date} at ${time}`);
-// 	}
-// })
-
 client.on('interactionCreate', (interaction) => {
 	if(!interaction.isChatInputCommand()) return;
 
 	if(interaction.commandName === 'embed') {
 		const embed = new EmbedBuilder()
 		.setTitle("Together")
-		.setDescription("Link to the offical Together App")
+		.setDescription("Alert members for event")
 		.setColor('0x0099FF')
 		.setURL('https://together.cyclic.app')
-		.setAuthor({name: 'Together', iconURL: 'https://ibb.co/VjsFGDg', url: 'https://together.cyclic.app'})
-		.setThumbnail('https://ibb.co/VjsFGDg')
+		.setAuthor({name: 'Together', iconURL: 'https://i.ibb.co/qyvH89N/logoicon.png', url: 'https://together.cyclic.app'})
+		.setThumbnail('https://i.ibb.co/qyvH89N/logoicon.png')
 		.addFields(
 			{ name: 'Field title', value: 'Some random value', inline: true },
 			{ name: 'Field title2', value: 'Some random value2', inline: true }
 		)
-		.setImage('https://ibb.co/VjsFGDg')
+		.setImage('https://i.ibb.co/qyvH89N/logoicon.png')
 		.setTimestamp()
-		.setFooter({ text: 'Some footer text', iconURL: 'https://ibb.co/VjsFGDg' })
+		.setFooter({ text: 'Some footer text', iconURL: 'https://i.ibb.co/qyvH89N/logoicon.png' })
 
-	interaction.reply({ embeds: [embed] });
+
+// For sending DMS to ALL users in the discord. Also uncomment line 77.
+
+// Get the guild from the interaction
+// const guild = client.guilds.cache.get(interaction.guildId);
+
+// Get all members in the guild
+// const members = guild.members.cache;
+
+// Loop through each member and send the embed
+// members.forEach((member) => {
+//   // Check if the member is a bot or the command author
+// if (member.user.bot || member.user.id === interaction.user.id) {
+//     return;
+// }
+
+// Send the embed to the member's DMs
+// member.user.send({ embeds: [embed] })
+//     .catch((error) => {
+//     console.error(`Failed to send message to ${member.user.username}: ${error}`);
+//     });
+// });
+
+// Testing for just for who initiates the slash command //////////////////
+		let user = client.users.cache.find(
+			(user) => user.id === interaction.user.id
+		);
+		user.send({embeds: [embed]});
+
+	interaction.reply({ content : 'Message sent!' });
 	}
+//////////////////////////////////////////////////////////////////////////
+	// For sending DMS to ALL users in the discord 
+	// interaction.reply({ content : 'Message sent!' });
 });
-
-client.on('messageCreate', (message) => {
-	if(message.content === 'embed'){
-		const embed = new EmbedBuilder()
-		.setTitle("Together")
-		.setDescription("Link to the offical Together App")
-		.setColor('0x0099FF')
-		.setURL('https://together.cyclic.app')
-		.setAuthor({name: 'Together', iconURL: 'https://ibb.co/VjsFGDg', url: 'https://together.cyclic.app'})
-		.setThumbnail('https://ibb.co/VjsFGDg')
-		.addFields(
-			{ name: 'Field title', value: 'Some random value', inline: true },
-			{ name: 'Field title2', value: 'Some random value2', inline: true }
-		)
-		.setImage('https://ibb.co/VjsFGDg')
-		.setTimestamp()
-		.setFooter({ text: 'Some footer text', iconURL: 'https://ibb.co/VjsFGDg' });
-
-		message.channel.send({ embeds: [embed] })
-	}
-})
 
 // Log in to Discord with your client's token
 client.login(TOKEN);
